3533607aa95710e5578f682271334743c60ca881
Ian Welsh <derekandclive1@googlemail.com>
2017-07-25 15:25:55 +0100

    Initial commit

---
+++ app/src/main/java/wizard/ian/android/fruitinventory/MainActivity.java
@@ -0,0 +1,105 @@
+package wizard.ian.android.fruitinventory;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtName, edtPice;
+    Button btnChoose, btnAdd, btnList;
+    ImageView imageView;
+
+    final int REQUEST_CODE_GALLERY = 999;
+
+    public static SQLiteHelper sqLiteHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+
+        //Create the db name
+        sqLiteHelper = new SQLiteHelper(this, "FoodDB.sqlite", null, 1);
+
+        sqLiteHelper.queryData("CREATE TABLE IF NOT EXISTS FOOD (Id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, price VARCHAR, image BLOG)");
+
+        //Attach a click to the btnChoose and het image form gallery into ImageView
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY
+                );
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_CODE_GALLERY){
+            if (grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
+            } else {
+                Toast.makeText(getApplicationContext(), "You don't have access to file location!",Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //Find the xml items
+    private void init(){
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtPice = (EditText) findViewById(R.id.edtPrice);
+        btnChoose = (Button) findViewById(R.id.btnChooseImage);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnList = (Button) findViewById(R.id.btnList);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+    }
+}
